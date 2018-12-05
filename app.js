c5729bb7072ae77dc941ac6651fb127210c450e3
KALBAY <gulzhamal-93@mail.ru>
2018-12-05 16:39:27 +0900

    s 수정했음

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-/* test: abcdefghijikkk */
-
+/* test: abdefghigjgjgjjikkk */session
 /* Session */
 const session = require('express-session');
 app.use(session({
