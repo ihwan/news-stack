8a7dc7f0c13f2cec66b57a291d5c9c5387a7f1fc
hyogyeong <33477461+hyogyeong@users.noreply.github.com>
2018-12-05 16:10:45 +0900

    주석 수정
    주석 코드를 수정하였습니다.

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
-/* test: abcdefghijikkk */
+/*
+
+hyogyeong */
+
 
 /* Session */
 const session = require('express-session');
